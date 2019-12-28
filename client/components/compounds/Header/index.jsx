870d2741b61905a0c89badc058e4e291dfb1bfe3
Praveen Bisht <prvnbist@gmail.com>
2019-12-29 00:58:19 +0530

    feat(Brand): Logo & Styles

--- client/components/compounds/Header/index.jsx
+++ client/components/compounds/Header/index.jsx
@@ -1,6 +1,21 @@
-import React from "react";
-import {} from "./styles";
+import React from 'react'
+import Link from 'next/link'
 
-const Header = () => <header>Header</header>;
+import { StyledHeader, Brand, Main } from './styles'
 
-export default Header;
+import Logo from '../../../assets/Logo'
+
+const Header = ({ home, children }) => (
+   <StyledHeader>
+      <Brand>
+         <Link href={home}>
+            <a>
+               <Logo />
+            </a>
+         </Link>
+      </Brand>
+      <Main>{children}</Main>
+   </StyledHeader>
+)
+
+export default Header
---
+++ client/components/compounds/Header/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.header`
+   height: 64px;
+   width: 100vw;
+   display: flex;
+   align-items: center;
+`
+export const Brand = styled.div`
+   width: 240px;
+   display: flex;
+   height: inherit;
+   padding: 0 16px;
+   align-items: center;
+   background: ${({ theme }) => theme.colors.denimBlue};
+`
+
+export const Main = styled.main`
+   display: flex;
+   height: inherit;
+   padding: 0 16px;
+   align-items: center;
+   width: calc(100vw - 240px);
+`
