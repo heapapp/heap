16bdd300e99162bb878d948cd855684fdb813611
Praveen Bisht <prvnbist@gmail.com>
2019-12-29 19:51:11 +0530

    styles(video card): with populated data

--- client/components/atoms/Video/index.jsx
+++ client/components/atoms/Video/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Router from 'next/router'
 
 import { Heading } from '../../index'
 
@@ -6,11 +7,16 @@ import { StyledVideo, Thumbnail, Details } from './styles'
 
 const Video = ({ data }) => {
    return (
-      <StyledVideo>
-         <Thumbnail></Thumbnail>
+      <StyledVideo onClick={() => Router.push(`/video/${data.id.videoId}`)}>
+         <Thumbnail>
+            <img
+               src={data.snippet.thumbnails.high.url}
+               alt={data.snippet.title}
+            />
+         </Thumbnail>
          <Details>
             <span></span>
-            <Heading type="h5">{data.title}</Heading>
+            <Heading type="h5">{data.snippet.title}</Heading>
          </Details>
       </StyledVideo>
    )
--- client/components/atoms/Video/styles.js
+++ client/components/atoms/Video/styles.js
@@ -1,13 +1,22 @@
 import styled from 'styled-components'
 
-export const StyledVideo = styled.div``
+export const StyledVideo = styled.div`
+   cursor: pointer;
+`
 
 export const Thumbnail = styled.div`
    height: auto;
-   padding-top: 56.25%;
+   overflow: hidden;
    border-radius: 8px;
+   padding-top: 56.25%;
    margin-bottom: 16px;
+   position: relative;
    background: ${({ theme }) => theme.colors.dark};
+   img {
+      position: absolute;
+      top: -16.5%;
+      width: 100%;
+   }
 `
 
 export const Details = styled.div`
